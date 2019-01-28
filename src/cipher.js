639639bc1eb1ced3dbe18c79943ea6c8c57b87e4
veridiem <jessicavga@gmail.com>
2019-01-28 12:29:10 -0200

    Primeira atualização js e html

---
+++ src/cipher.js
@@ -0,0 +1,26 @@
+
+var input1 = document.getElementById("chave1").innerHTML;
+var input2 = document.getElementbyId("code").innerHTML;
+input1.focus();
+var input3 = document.querySelectorAll("input");
+
+
+var button = document.querySelector("button");
+
+var alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "w", "x", "y", "z"];
+
+function codifica(){
+  
+   input3.value()= "";
+  
+}
+
+button.onclick() = codifica;
+
+
+function decodifica(){
+}
+  
+button.onclick() = decodifica;
+
+
