3454acc0f1cf8313493e8871538debea754d044a
veridiem <jessicavga@gmail.com>
2019-01-29 16:49:01 -0200

    Versão com decode ainda com bugs, falta espaço funcionar

--- src/cipher.js
+++ src/cipher.js
@@ -7,19 +7,21 @@ function codificar(){
     var deslocamento = parseInt(document.getElementById("chave1").value);
     var mensagemEnviada = document.getElementById("string").value;
 
-    let result;
+    let result = "";
     let mensagemCodificada;
     for(let i = 0; i < mensagemEnviada.length; i++) {
 
-        if(mensagemEnviada[i].charCodeAt() >= 65 && mensagemEnviada[i].charCodeAt() <= 90){
-            mensagemCodificada = ((mensagemEnviada[i].charCodeAt() - 65 + deslocamento) % 26) + 65;
-            result += String.fromCharCode(mensagemCodificada); 
-        }
-
-        else if(mensagemEnviada[i].charCodeAt() >= 97 && mensagemEnviada[i].charCodeAt() <= 122){
-            mensagemCodificada = ((mensagemEnviada[i].charCodeAt() - 97 + deslocamento) % 26) + 97;
-            result += String.fromCharCode(mensagemCodificada); 
-        }
+            if(mensagemEnviada[i].charCodeAt() >= 65 && mensagemEnviada[i].charCodeAt() <= 90){
+                mensagemCodificada = ((mensagemEnviada[i].charCodeAt() - 65 + deslocamento) % 26) + 65;
+                result += String.fromCharCode(mensagemCodificada); 
+            }
+    
+            else if(mensagemEnviada[i].charCodeAt() >= 97 && mensagemEnviada[i].charCodeAt() <= 122){
+                mensagemCodificada = ((mensagemEnviada[i].charCodeAt() - 97 + deslocamento) % 26) + 97;
+                result += String.fromCharCode(mensagemCodificada); 
+            }
+        
+        
     }
         
     document.getElementById("codificada").value = result;
@@ -31,17 +33,17 @@ function decodificar(){
     var deslocamento2 = parseInt(document.getElementById("chave2").value);
     var mensagemEnviada2 = document.getElementById("string2").value;
 
-    let result2;
+    let result2 = "";
     let mensagemDecodificada;
     for(let i = 0; i < mensagemEnviada2.length; i++) {
 
         if(mensagemEnviada2[i].charCodeAt() >= 65 && mensagemEnviada2[i].charCodeAt() <= 90){
-            mensagemDecodificada = ((mensagemEnviada2[i].charCodeAt() - 65 + deslocamento2) % 26) + 65;
+            mensagemDecodificada = ((mensagemEnviada2[i].charCodeAt() - 65 - deslocamento2) % 26) + 65;
             result2 += String.fromCharCode(mensagemDecodificada); 
         }
 
         else if(mensagemEnviada2[i].charCodeAt() >= 97 && mensagemEnviada2[i].charCodeAt() <= 122){
-            mensagemDecodificada = ((mensagemEnviada2[i].charCodeAt() - 97 + deslocamento2) % 26) + 97;
+            mensagemDecodificada = ((mensagemEnviada2[i].charCodeAt() - 97 - deslocamento2) % 26) + 97;
             result2 += String.fromCharCode(mensagemDecodificada); 
         }
     }
